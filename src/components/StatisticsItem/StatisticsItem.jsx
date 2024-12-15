e106a19e487a49db2ba89b40bcd20c5f160ab8d1
Serhii Mordvaniuk <sm636332@gmail.com>
2024-12-15 20:41:44 +0100

    fix icons in 2 task

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,11 +1,11 @@
-import style from './StatisticsItem.module.css';
+import styles from './StatisticsItem.module.css';
 
 const StatisticsItem = ({ title, total, icon: Icon }) => {
   return (
     <>
-      <Icon />
-      <span className={style.counter}>{total}</span>
-      <p className={style.text}>{title}</p>
+      <Icon className={styles.icon} />
+      <span className={styles.counter}>{total}</span>
+      <p className={styles.text}>{title}</p>
     </>
   );
 };
